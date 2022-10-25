ca64cabe292b1a5a77bfb2a286890921549ae247
gailazar300 <gail@jfrog.com>
2022-10-25 17:14:37 +0300

    remove lib.jfrog.repos.credentials.js and return credentials getters

--- src/main/java/io/jenkins/plugins/jfrog/configuration/Credentials.java
+++ src/main/java/io/jenkins/plugins/jfrog/configuration/Credentials.java
@@ -11,9 +11,9 @@ import java.io.Serializable;
  */
 public class Credentials implements Serializable {
     public static final Credentials EMPTY_CREDENTIALS = new Credentials(StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY);
-    private final Secret username;
-    private final Secret password;
-    private final Secret accessToken;
+    private Secret username;
+    private Secret password;
+    private Secret accessToken;
 
     /**
      * Main constructor
@@ -38,5 +38,20 @@ public class Credentials implements Serializable {
 
     public String getAccessToken() {
         return Secret.toString(accessToken);
+    }
+
+    @SuppressWarnings("unused")
+    public void setAccessToken(Secret accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    @SuppressWarnings("unused")
+    public void setUsername(Secret username) {
+        this.username = username;
+    }
+
+    @SuppressWarnings("unused")
+    public void setPassword(Secret password) {
+        this.password = password;
     }
 }
