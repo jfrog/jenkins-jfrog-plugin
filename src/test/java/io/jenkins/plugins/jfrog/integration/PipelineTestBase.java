d72dd09b24da0364c4d88d68c534c51a0f779796
gailazar300 <gail@jfrog.com>
2022-12-22 17:55:42 +0200

    rollback cred

--- src/test/java/io/jenkins/plugins/jfrog/integration/PipelineTestBase.java
+++ src/test/java/io/jenkins/plugins/jfrog/integration/PipelineTestBase.java
@@ -162,6 +162,11 @@ public class PipelineTestBase {
         addCreds(store, CredentialsScope.GLOBAL, "credentials");
     }
 
+    private static void addCreds(CredentialsStore store, CredentialsScope scope, String id) throws IOException {
+        // For purposes of this test we do not care about domains.
+        store.addCredentials(Domain.global(), new UsernamePasswordCredentialsImpl(scope, id, null, ARTIFACTORY_USERNAME, ARTIFACTORY_PASSWORD));
+    }
+
     private static CredentialsStore lookupStore(ModelObject object) {
         Iterator<CredentialsStore> stores = CredentialsProvider.lookupStores(object).iterator();
         assertTrue(stores.hasNext());
