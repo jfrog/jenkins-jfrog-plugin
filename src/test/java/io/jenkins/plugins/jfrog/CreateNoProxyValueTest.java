55687d6fa8bf1a3cbaba2c952d88b09c9abc9809
Nathan Amiel <59163863+Nathan770@users.noreply.github.com>
2024-10-07 16:03:14 +0300

    Update CreateNoProxyValueTest.java
    added test

--- src/test/java/io/jenkins/plugins/jfrog/CreateNoProxyValueTest.java
+++ src/test/java/io/jenkins/plugins/jfrog/CreateNoProxyValueTest.java
@@ -31,6 +31,7 @@ public class CreateNoProxyValueTest {
                 new Object[]{"   artifactory.jfrog.io    \n  \r     artifactory1.jfrog.io;artifactory2.jfrog.io    \n      artifactory3.jfrog.io | artifactory4.jfrog.io    \n      artifactory5.jfrog.io ", "artifactory.jfrog.io,artifactory1.jfrog.io,artifactory2.jfrog.io,artifactory3.jfrog.io,artifactory4.jfrog.io,artifactory5.jfrog.io"},
                 new Object[]{"\r\n", ""},
                 new Object[]{";;;", ""},
+                new Object[]{",,,", ""},
                 new Object[]{"artifactory.jfrog.io;", "artifactory.jfrog.io"},
                 new Object[]{"artifactory.jfrog.io,artifactory1.jfrog.io", "artifactory.jfrog.io,artifactory1.jfrog.io"},
                 new Object[]{"artifactory.jfrog.io;artifactory1.jfrog.io;artifactory2.jfrog.io;artifactory3.jfrog.io", "artifactory.jfrog.io,artifactory1.jfrog.io,artifactory2.jfrog.io,artifactory3.jfrog.io"},
